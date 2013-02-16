69ddde984d64c8787cee161e12824b4bf3af289e
Romain Naour <romain.naour@openwide.fr>
2013-02-16 14:15:08 +0100

    Mini2440: fix LONGHELP
    CONFIG_LONGHELP has been remplaced by CONFIG_SYS_LONGHELP

--- include/configs/mini2440.h
+++ include/configs/mini2440.h
@@ -119,7 +119,7 @@
 /*
  * Miscellaneous configurable options
  */
-#define CONFIG_LONGHELP
+#define CONFIG_SYS_LONGHELP
 #define CONFIG_SYS_PROMPT	"MINI2440 => "
 #define CONFIG_SYS_CBSIZE	256
 #define CONFIG_SYS_PBSIZE	(CONFIG_SYS_CBSIZE+sizeof(CONFIG_SYS_PROMPT)+16)
